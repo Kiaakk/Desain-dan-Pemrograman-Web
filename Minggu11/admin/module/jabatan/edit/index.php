0436f0190ac0b06fe111fdfd62b8128e7a4c4234
Kiaakk <krisna.wiliam26@gmail.com>
2024-11-12 22:54:03 +0700

    Minggu 11: Desain dan Pemrograman Web

---
+++ Minggu11/admin/module/jabatan/edit/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Edit Jabatan</title>
+        <!-- Tambahkan CSS Bootstrap jika belum ada -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <div class="container-fluid">
+            <div class="row">
+
+                <?php
+                require 'admin/template/menu.php';
+                require 'config/koneksi.php'; // Pastikan koneksi database disertakan
+                
+                $id = $_GET['id'];
+                $query = "SELECT * FROM jabatan WHERE id = '$id'";
+                $result = mysqli_query($koneksi, $query);
+                $row = mysqli_fetch_assoc($result);
+                ?>
+
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                        <h1 class="h2">Edit Jabatan</h1>
+                    </div>
+
+                    <div class="card col-md-6">
+                        <div class="card-header">
+                            Form Edit Jabatan
+                        </div>
+
+                        <div class="card-body">
+                            <form action="fungsi/edit.php?jabatan=edit" method="POST">
+                                <input type="hidden" value="<?php echo $row['id']; ?>" name="id">
+
+                                <div class="mb-3">
+                                    <label for="jabatan" class="form-label">Jabatan</label>
+                                    <input type="text" class="form-control" name="jabatan" value="<?= $row['jabatan']; ?>">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="keterangan" class="form-label">Keterangan</label>
+                                    <textarea class="form-control" name="keterangan"><?= $row['keterangan']; ?></textarea>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-floppy-o" aria-hidden="true"></i> Ubah
+                                </button>
+                                <a href="index.php?page=jabatan" class="btn btn-secondary">
+                                    <i class="fa fa-times" aria-hidden="true"></i> Batal
+                                </a>
+                            </form>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+
+        <!-- Tambahkan JS Bootstrap jika belum ada -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
